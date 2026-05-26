1f2ac27600ec0350a4aaccb2de42893b55ea6fa8
EmilianoPadilla <emilianopadillarobles@gmail.com>
2026-05-25 23:14:52 -0600

    fixed dependencies file

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -2,7 +2,7 @@ from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 
-from .database.database import get_db
+from src.database.database import get_db
 from .models.users import UserDB
 from .auth.jwt_handler import verify_token
 
