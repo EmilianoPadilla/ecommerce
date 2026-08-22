1bf1a1742a5e1251083c66496d8269506911074a
EmilianoPadilla <emilianopadillarobles@gmail.com>
2026-08-22 14:38:05 -0600

    describe changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers import users, orders, orderitems, products, categories, auth
 
 from src.database.database import engine
@@ -17,10 +18,20 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to our e-commerce!"}
+    return {"message": "Welcome to our e-commerce! Have a nice day! "}
 
 app.include_router(users.router)
 app.include_router(orders.router)
